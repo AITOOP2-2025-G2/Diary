7e6f4df8e172484d43b7adcc566dcb012c99fcd6
jumpei28 <k24074kk@aitech.ac.jp>
2025-10-16 16:37:15 +0900

    commit final

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from diaries.DiarySample import DiarySample
 from diaries.Diaryebi import Diaryebi
+from diaries.JumpeiDiary import JumpeiDiary
+from diaries.kurotsukaDiary import kurotsukaDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [DiarySample(), 
+           JumpeiDiary(),
            Diaryebi(),
+        kurotsukaDiary(),
            ] 
-from diaries.JumpeiDiary import JumpeiDiary
 
-# ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), JumpeiDiary(), ]
+
+
 
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
     print(d.get_summary())
     print(d.get_author())
-    print(
+    print()
